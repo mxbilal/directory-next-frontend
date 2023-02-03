9d709dbee6a00827e1315a4dd86797b373d49111
Muhammad-Bilal89 <mbilal@selteq.net>
2023-02-03 11:49:16 +0500

    test

---
+++ src/Components/DNavbar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DNavbar = () => {
+  return (
+    <div className="d-navbar">
+      <p className="logo-text">Directory</p>
+      <ul>
+        <li>Explore</li>
+        <li>Help</li>
+        <li>Blog</li>
+        <li>Login</li>
+      </ul>
+      <p>Sign Up</p>
+    </div>
+  );
+};
+
+export default DNavbar;
